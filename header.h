18fe227239f483138a0a7518d60f0a98957a4808
1rens1 <rein.satyawarman@gmail.com>
2025-04-28 13:35:01 +0700

    #3

--- header.h
+++ header.h
@@ -2,3 +2,8 @@
 #define IR_RIGHT 10
 #define US_TRIG 11
 #define US_ECHO 12
+
+#define MOTOR_LEFT_IN1 7
+#define MOTOR_LEFT_IN2 8
+#define MOTOR_RIGHT_IN3 6
+#define MOTOR_RIGHT_IN4 5
--- ye-olde-linefollower.ino
+++ ye-olde-linefollower.ino
@@ -1,14 +1,25 @@
 #include "header.h"
 
-void setup() {
-  Serial.begin(9600);
+void setupPinModes() {
   pinMode(IR_LEFT, OUTPUT);
   pinMode(IR_RIGHT, OUTPUT);
   pinMode(US_ECHO, INPUT);
-  pinMode(US_TRIG, OUTPUT);
+
+  pinMode(MOTOR_LEFT_IN1, OUTPUT);
+  pinMode(MOTOR_LEFT_IN2, OUTPUT);
+  pinMode(MOTOR_RIGHT_IN3, OUTPUT);
+  pinMode(MOTOR_RIGHT_IN4, OUTPUT);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  setupPinModes();
 
   Serial.println("Hello world!");
-  Serial.println(IR_LEFT);
+  
+  digitalWrite(MOTOR_LEFT_IN1, 100);
+  digitalWrite(MOTOR_RIGHT_IN3, 100);
 }
 
 float duration = 0;
@@ -20,10 +31,11 @@ void loop() {
   digitalWrite(US_TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(US_TRIG, LOW);
-  
+
   duration = pulseIn(US_ECHO, HIGH);
   distance = (duration * 0.0343) / 2;
 
   Serial.print("Distance = ");
   Serial.println(distance);
+
 }
